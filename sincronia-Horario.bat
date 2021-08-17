ac7f15a3da86367e0cfd2e321f5c8b6676105805
Mateus Strozzi <66045441+mateustrozzi@users.noreply.github.com>
2021-08-17 15:31:41 -0300

    Update sincronia-Horario.bat
    Adiciona chamada de msg.vbs

--- sincronia-Horario.bat
+++ sincronia-Horario.bat
@@ -1,7 +1,9 @@
-w32tm /config /syncfromflags:manual /manualpeerlist:0.pool.ntp.org
+w32tm /config /syncfromflags:manual /manualpeerlist:0.pool.
+tp.org
 net stop w32time
 net start w32time
 w32tm /resync /rediscover
+call msg.vbs
 exit
 
 
